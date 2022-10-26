b7465e3949b015bebe3b0e050e4953ca74492c44
Aleksejs Semjonovs <yelowcat@gmail.com>
2022-10-26 17:22:43 +0300

    Add project files.

---
+++ Dynamic/Program.cs
@@ -0,0 +1,34 @@
+﻿
+using Dynamic;
+
+using System.Dynamic;
+
+//dynamic a;
+//a = 2;
+//a++;
+//Console.WriteLine(a);
+
+//a = "Hello world!!!";
+//Console.WriteLine(a);
+//a++;
+//a.Member = 1;
+
+//a = new SomeClass();
+//a.Guutt = "Super gut!";
+//Console.WriteLine(a.Guutt);
+
+
+dynamic person = new ExpandoObject();
+person.Name = "Tom";
+person.Age = 46;
+person.Languages = new List<string> { "english", "german", "french" };
+
+Console.WriteLine($"{person.Name} - {person.Age}");
+foreach (var lang in person.Languages)
+    Console.WriteLine(lang);
+
+// объявляем метод
+person.IncrementAge = (Action<int>)(x => person.Age += x);
+person.IncrementAge(6); // увеличиваем возраст на 6 лет
+Console.WriteLine($"{person.Name} - {person.Age}");
+
